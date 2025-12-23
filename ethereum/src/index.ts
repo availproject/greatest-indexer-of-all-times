f0429c100fbbab1c376e15253cd2df362fbebe01
Sasa Prsic <s.prsic@gmail.com>
2025-12-23 12:31:58 +0100

    Add log to ponder insert.

--- ethereum/src/index.ts
+++ ethereum/src/index.ts
@@ -1,108 +1,123 @@
 import {
-  decodeFunctionData,
-  decodeAbiParameters,
-  Hex,
-  toHex,
-  keccak256,
-  concat,
-  GetProofReturnType,
+    decodeFunctionData,
+    decodeAbiParameters,
+    Hex,
+    toHex,
+    keccak256,
+    concat,
+    GetProofReturnType,
 } from "viem";
-import { ponder } from "ponder:registry";
+import {ponder} from "ponder:registry";
 import schema from "ponder:schema";
-import { BridgeImplAbi } from "../abis/BridgeImplAbi";
-import { DecodedResult, STATUS } from "./types";
-import { replaceBigInts } from "ponder";
+import {BridgeImplAbi} from "../abis/BridgeImplAbi";
+import {DecodedResult, STATUS} from "./types";
+import {replaceBigInts} from "ponder";
 
-ponder.on("AvailBridgeV1:MessageReceived", async ({ event, context }) => {
-  const tx = await context.client.getTransaction({
-    hash: event.transaction.hash,
-  });
+ponder.on("AvailBridgeV1:MessageReceived", async ({event, context}) => {
+    const tx = await context.client.getTransaction({
+        hash: event.transaction.hash,
+    });
 
-  //these are the idle finance execTransaction events, which break our flow.
-  if (tx.input.startsWith("0x6a761202")) {
-    console.log("Skipping unknown function signature 0x6a761202");
-    return;
-  }
+    //these are the idle finance execTransaction events, which break our flow.
+    if (tx.input.startsWith("0x6a761202")) {
+        console.log("Skipping unknown function signature 0x6a761202");
+        return;
+    }
 
-  const decoded = decodeFunctionData({
-    abi: BridgeImplAbi,
-    data: tx.input,
-  }) as unknown as DecodedResult<Hex>;
+    const decoded = decodeFunctionData({
+        abi: BridgeImplAbi,
+        data: tx.input,
+    }) as unknown as DecodedResult<Hex>;
 
-  const assetStructEncoded = decoded.args[0]!.data;
-  const [assetId, amount] = decodeAbiParameters(
-    [
-      { name: "assetId", type: "bytes32" },
-      { name: "amount", type: "uint256" },
-    ],
-    assetStructEncoded,
-  );
+    const assetStructEncoded = decoded.args[0]!.data;
+    const [assetId, amount] = decodeAbiParameters(
+        [
+            {name: "assetId", type: "bytes32"},
+            {name: "amount", type: "uint256"},
+        ],
+        assetStructEncoded,
+    );
 
-  await context.db.insert(schema.bridgeEvent).values({
-    sender: event.args.from,
-    receiver: event.args.to,
-    messageId: event.args.messageId,
-    //for compatibility with the rust indexer, we need to store it as text (numeric(78) represenation of bigint vs native bigint)
-    amount: amount.toString(),
-    eventType: "MessageReceived",
-    status: STATUS.BRIDGED,
-    blockNumber: Number(event.block.number),
-    sourceBlockHash: event.block.hash,
-    sourceTransactionHash: event.transaction.hash,
-  });
+    console.log({
+        type: "MessageReceived",
+        "amount": amount,
+        "from": event.args.from,
+        "to": event.args.to
+    });
+
+    await context.db.insert(schema.bridgeEvent).values({
+        sender: event.args.from,
+        receiver: event.args.to,
+        messageId: event.args.messageId,
+        //for compatibility with the rust indexer, we need to store it as text (numeric(78) represenation of bigint vs native bigint)
+        amount: amount.toString(),
+        eventType: "MessageReceived",
+        status: STATUS.BRIDGED,
+        blockNumber: Number(event.block.number),
+        sourceBlockHash: event.block.hash,
+        sourceTransactionHash: event.transaction.hash,
+    });
 });
 
-ponder.on("AvailBridgeV1:MessageSent", async ({ event, context }) => {
-  const tx = await context.client.getTransaction({
-    hash: event.transaction.hash,
-  });
+ponder.on("AvailBridgeV1:MessageSent", async ({event, context}) => {
+    const tx = await context.client.getTransaction({
+        hash: event.transaction.hash,
+    });
 
-  const messageId = event.args.messageId;
-  const storageSlot = 1n;
+    const messageId = event.args.messageId;
+    const storageSlot = 1n;
 
-  const messageIdBytes = toHex(messageId, { size: 32 });
-  const storageSlotBytes = toHex(storageSlot, { size: 32 });
-  const storageKey = keccak256(concat([messageIdBytes, storageSlotBytes]));
+    const messageIdBytes = toHex(messageId, {size: 32});
+    const storageSlotBytes = toHex(storageSlot, {size: 32});
+    const storageKey = keccak256(concat([messageIdBytes, storageSlotBytes]));
 
-  const proof = await context.client.getProof({
-    address: context.contracts.AvailBridgeV1.address,
-    storageKeys: [storageKey],
-    blockNumber: event.block.number,
-  });
+    const proof = await context.client.getProof({
+        address: context.contracts.AvailBridgeV1.address,
+        storageKeys: [storageKey],
+        blockNumber: event.block.number,
+    });
 
-  const decoded = decodeFunctionData({
-    abi: BridgeImplAbi,
-    data: tx.input,
-  });
+    const decoded = decodeFunctionData({
+        abi: BridgeImplAbi,
+        data: tx.input,
+    });
 
-  //no choice but to typecast since jsonb does not support bigint natively
-  const bigIntAdjustedProof = replaceBigInts(proof, (v) =>
-    String(v),
-  ) as unknown as GetProofReturnType;
+    //no choice but to typecast since jsonb does not support bigint natively
+    const bigIntAdjustedProof = replaceBigInts(proof, (v) =>
+        String(v),
+    ) as unknown as GetProofReturnType;
 
-  await context.db
-    .insert(schema.bridgeEvent)
-    .values({
-      messageId: event.args.messageId,
-      sender: event.args.from,
-      receiver: event.args.to,
-      amount: decoded.args[1] as string,
-      eventType: "MessageSent",
-      proof: bigIntAdjustedProof,
-      status: STATUS.IN_PROGRESS,
-      blockNumber: Number(event.block.number),
-      sourceBlockHash: event.block.hash,
-      sourceTransactionHash: event.transaction.hash,
-    })
-    .onConflictDoUpdate((existing) => ({
-      sender: event.args.from,
-      receiver: event.args.to,
-      amount: decoded.args[1] as string,
-      eventType: "MessageSent",
-      proof: bigIntAdjustedProof,
-      status: STATUS.IN_PROGRESS,
-      blockNumber: Number(event.block.number),
-      sourceBlockHash: event.block.hash,
-      sourceTransactionHash: event.transaction.hash,
-    }));
+    let amount = decoded.args[1] as string;
+    console.log({
+        type: "MessageSent",
+        "amount": amount,
+        "from": event.args.from,
+        "to": event.args.to
+    });
+
+    await context.db
+        .insert(schema.bridgeEvent)
+        .values({
+            messageId: event.args.messageId,
+            sender: event.args.from,
+            receiver: event.args.to,
+            amount: amount,
+            eventType: "MessageSent",
+            proof: bigIntAdjustedProof,
+            status: STATUS.IN_PROGRESS,
+            blockNumber: Number(event.block.number),
+            sourceBlockHash: event.block.hash,
+            sourceTransactionHash: event.transaction.hash,
+        })
+        .onConflictDoUpdate((existing) => ({
+            sender: event.args.from,
+            receiver: event.args.to,
+            amount: decoded.args[1] as string,
+            eventType: "MessageSent",
+            proof: bigIntAdjustedProof,
+            status: STATUS.IN_PROGRESS,
+            blockNumber: Number(event.block.number),
+            sourceBlockHash: event.block.hash,
+            sourceTransactionHash: event.transaction.hash,
+        }));
 });
